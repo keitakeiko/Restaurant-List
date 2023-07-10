ff9fc5ed594bbaace6dff4f186f2223b04bc0bbe
Keiko <keitakeiko@hotmail.com>
2023-07-10 21:51:51 +0800

    feat: add user login / register page and add bootswatch style

--- routes/index.js
+++ routes/index.js
@@ -9,9 +9,15 @@ const home = require('./modules/home')
 // 引入 restaurants 模組程式碼
 const restaurants = require('./modules/restaurants')
 
-// 將網址結構符合 / 字串的 request 導向 home 模組 
-router.use('/', home)
+// 引入 users 模組程式碼
+const users = require('./modules/users')
+
+// 將網址結構符合 /restaurants 字串的 restaurants 導向 home 模組 
 router.use('/restaurants', restaurants)
+// 將網址結構符合 / 字串的 request 導向 home 模組 
+router.use('/users', users)
+// 將網址結構符合 /users 字串的 request 導向 users 模組 
+router.use('/', home)
 
 // 匯出路由器
 module.exports = router
